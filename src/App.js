880747c5a9467a38dd48ba878457fd19cf4e78ef
Fred-S <fredvantage@gmail.com>
2021-01-26 17:49:35 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
     return (
         <div className='App'>
-            )
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code> src / App.js </code> and save to reload.{' '}
-                </p>{' '}
-                <a
-                    className='App-link'
-                    href='https://fredsoloy'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    Fred Soloy{' '}
-                </a>{' '}
-            </header>{' '}
+            <div className='content'>
+                <h1>App Component</h1>
+            </div>
         </div>
     )
 }
