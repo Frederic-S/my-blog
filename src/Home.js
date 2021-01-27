62cf1a5073b847305ba2505c410898d28925c8e3
Fred-S <fredvantage@gmail.com>
2021-01-27 06:26:00 +0100

    added css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Hello, my friends')
+    }
     return (
         <div className='home'>
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     )
 }
